81ba94e88cb0cdd34af1d27ca966f7fed80988d0
bock-instead <139914246+markboeckenstedt@users.noreply.github.com>
2024-07-10 13:06:47 -0500

    Add all products route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,10 @@
 class ProductsController < ApplicationController
+  def all_products
+    products = Product.all
+    render json: [
+      { id: products[0].id, name: products[0].name, price: products[0].price },
+      { id: products[1].id, name: products[1].name, price: products[1].price },
+      { id: products[2].id, name: products[2].name, price: products[2].price },
+    ]
+  end
 end
